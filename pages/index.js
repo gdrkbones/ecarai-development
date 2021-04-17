d518d52e6fdc5621f2d40c1ee46f25b667fb3a18
Julio Cesar Santiesteban <gdrkbones@gmail.com>
2021-04-17 12:08:46 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,36 @@ import Image from "next/image";
 import { createClient } from "contentful";
 
 export const getStaticProps = async (context) => {
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-  });
+  // const client = createClient({
+  //   space: process.env.CONTENTFUL_SPACE_ID,
+  //   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  // });
 
-  let plants = await client.getEntries({
-    content_type: "plant",
-  });
+  // let plants = await client.getEntries({
+  //   content_type: "plant",
+  // });
+  let plants;
+  let plantsInfo;
+  if (process.env.LOCAL_DEVELOPMENT) {
+    plantsInfo = require("../mock/data/plants.json");
+    plants = plantsInfo.items.map((item) => ({
+      name: item.fields.name,
+      iconUrl: item.fields.icon.fields.file.url,
+    }));
+  } else {
+    const client = createClient({
+      space: process.env.CONTENTFUL_SPACE_ID,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    });
+    plantsInfo = await client.getEntries({
+      content_type: "plant",
+    });
+    plants = plantsInfo.items.map((item) => ({
+      name: item.fields.name,
+      iconUrl: `https:${item.fields.icon.fields.file.url}`,
+    }));
+  }
+
   return {
     props: {
       plants: plants,
@@ -45,14 +67,10 @@ const Home = ({ plants }) => {
     <Grid className={root}>
       <Navbar />
       <Grid className={plantsContainer}>
-        {plants.items.map((item) => (
-          <Grid className={plant} key={item.fields.name}>
-            <Typography>{item.fields.name}</Typography>
-            <Image
-              src={"https:" + item.fields.icon.fields.file.url}
-              width={64}
-              height={64}
-            />
+        {plants.map((item) => (
+          <Grid className={plant} key={item.name}>
+            <Typography>{item.name}</Typography>
+            <Image src={item.iconUrl} width={64} height={64} />
           </Grid>
         ))}
       </Grid>
